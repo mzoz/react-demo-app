94eab2ce363c28d4e03fd2bbfe086737cfdeefd7
mz <zmyunsw@gmail.com>
2019-10-18 17:11:37 +1030

    Updates

---
+++ precache-manifest.acefe5ffd82d254773334fa2cafb11d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4fc5482b8f3d787618a775d41faf20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff65a59690b2d953f6df",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "eb59c0e5f657362e91e7",
+    "url": "/static/js/2.3a4dbbb5.chunk.js"
+  },
+  {
+    "revision": "ff65a59690b2d953f6df",
+    "url": "/static/js/main.6b4dcbe3.chunk.js"
+  },
+  {
+    "revision": "890eb6b34760575430a9",
+    "url": "/static/js/runtime-main.b66b9974.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
